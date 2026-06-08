3bbcd8ec64b059af1a91bde220863b4442a1969b
Max Belich <maxbelich@web.de>
2026-06-08 14:57:16 +0200

    feat: add pokemon type badges

--- script/templates.js
+++ script/templates.js
@@ -17,6 +17,30 @@ function getPokemonCardTemplate(pokemonIndex) {
       <span>#${pokemonDetails[pokemonIndex].id}</span>
       <h2>${pokemonDetails[pokemonIndex].name}</h2>
       <img src="${pokemonDetails[pokemonIndex].sprites.front_default}" alt="${pokemonDetails[pokemonIndex].name}">
+      <div class="pokemon_types">
+        ${getPokemonTypesTemplate(pokemonIndex)}
+      </div>
     </div>
   `;
+}
+
+function getPokemonTypesTemplate(pokemonIndex) {
+  let typesTemplate = "";
+
+  for (
+    let typeIndex = 0;
+    typeIndex < pokemonDetails[pokemonIndex].types.length;
+    typeIndex++
+  ) {
+    let typeName = pokemonDetails[pokemonIndex].types[typeIndex].type.name;
+    let typeColor = typeColors[typeName] || "#777";
+
+    typesTemplate += /*html*/ `
+      <span class="pokemon_type" style="background-color: ${typeColor}">
+        ${typeName}
+      </span>
+    `;
+  }
+
+  return typesTemplate;
 }
