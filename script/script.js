5214e8e55e152ce6a777424e3c5ab4c655540ea9
Max Belich <maxbelich@web.de>
2026-06-09 16:47:36 +0200

    refactor: move pokemon search logic into separate file

--- script/script.js
+++ script/script.js
@@ -213,68 +213,6 @@ function renderLoadMoreButtonDefault() {
   loadMoreButtonRef.innerText = "Load More";
 }
 
-function handlePokemonSearch() {
-  const searchInputRef = document.getElementById("pokemon_search_input");
-  let searchValue = searchInputRef.value.trim().toLowerCase();
-  let cleanedSearchValue = searchValue.replace("#", "");
-  let isIdSearch = /^\d+$/.test(cleanedSearchValue);
-
-  if (searchValue === "") {
-    hidePokemonSearchError();
-    renderPokemonCards();
-    return;
-  }
-
-  if (!isIdSearch && searchValue.length < 3) {
-    showPokemonSearchError();
-    renderPokemonCards();
-    return;
-  }
-
-  hidePokemonSearchError();
-
-  let filteredPokemonIndexes = getFilteredPokemonIndexes(searchValue);
-  renderPokemonCards(filteredPokemonIndexes);
-}
-
-function showPokemonSearchError() {
-  const searchErrorRef = document.getElementById("pokemon_search_error");
-
-  searchErrorRef.classList.remove("invisible");
-}
-
-function hidePokemonSearchError() {
-  const searchErrorRef = document.getElementById("pokemon_search_error");
-
-  searchErrorRef.classList.add("invisible");
-}
-
-function getFilteredPokemonIndexes(searchValue) {
-  let filteredPokemonIndexes = [];
-  let cleanedSearchValue = searchValue.replace("#", "");
-
-  for (
-    let pokemonIndex = 0;
-    pokemonIndex < pokemonDetails.length;
-    pokemonIndex++
-  ) {
-    let pokemon = pokemonDetails[pokemonIndex];
-    let pokemonName = pokemon.name.toLowerCase();
-    let pokemonId = String(pokemon.id);
-    let formattedPokemonId = formatPokemonId(pokemon.id);
-
-    if (
-      pokemonName.includes(searchValue) ||
-      pokemonId === cleanedSearchValue ||
-      formattedPokemonId === cleanedSearchValue
-    ) {
-      filteredPokemonIndexes.push(pokemonIndex);
-    }
-  }
-
-  return filteredPokemonIndexes;
-}
-
 function formatPokemonId(id) {
   return String(id).padStart(3, "0");
 }
